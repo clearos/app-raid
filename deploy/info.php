bf9c43dc0a67d6ce64335c558f508c22d0130847
Roman Kosnar <kosnar@apeko.cz>
2014-05-12 09:09:59 +0200

    add parameter to general setting 'Send mail condition' with values 'On failure', 'Always'

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'raid';
-$app['version'] = '1.1.6';
+$app['version'] = '1.1.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
