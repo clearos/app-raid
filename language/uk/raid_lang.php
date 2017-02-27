e53a98eccf9feaba516881c718c878851d86551f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:10:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'Додати пристрій';
+$lang['raid_always'] = 'завжди';
+$lang['raid_app_description'] = 'Програмний RAID надає інформацію про стан, звітності та адміністративні дії для програмного забезпечення RAID.';
+$lang['raid_app_name'] = 'RAID Manager Software';
+$lang['raid_array'] = 'масив';
+$lang['raid_clean'] = 'чистий';
+$lang['raid_confirm_add'] = 'Будь ласка, підтвердіть, що ви хотіли б додати блок пристрою% S в% S в масиві RAID.';
+$lang['raid_confirm_remove'] = 'Будь ласка, підтвердіть, що ви хочете видалити блок пристрою% S з% S масиву RAID.';
+$lang['raid_degraded'] = 'Degraded';
+$lang['raid_device'] = 'пристрій';
+$lang['raid_email_notification'] = 'Повідомлення по електронній пошті';
+$lang['raid_every_30_minutes'] = 'Кожні 30 хвилин';
+$lang['raid_every_4_hours'] = 'Кожні 4 години';
+$lang['raid_every_minute'] = 'кожну хвилину';
+$lang['raid_frequency'] = 'частота';
+$lang['raid_level'] = 'рівень';
+$lang['raid_monitor_raid'] = 'монітор RAID';
+$lang['raid_mount'] = 'гора';
+$lang['raid_no_support'] = 'Ні підтримуваних RAID-масиви, не знайдено.';
+$lang['raid_not_degraded'] = 'Масив не в неробочому стані.';
+$lang['raid_on_failure'] = 'У разі невдачі';
+$lang['raid_raid_status'] = 'статус RAID';
+$lang['raid_raid_summary'] = 'RAID Резюме';
+$lang['raid_remove'] = 'видалення';
+$lang['raid_send_mail'] = 'Надіслати повідомлення по електронній пошті';
+$lang['raid_software_raid'] = 'Програмне забезпечення RAID';
+$lang['raid_sync_pending'] = 'Синхронізація в очікуванні';
+$lang['raid_sync_scheduled'] = 'Синхронізація за розкладом';
+$lang['raid_syncing'] = 'йде синхронізація';
+$lang['raid_test_email'] = 'Надіслати тестове повідомлення електронної пошти';
+$lang['raid_test_sent_to:'] = 'Успіх. Повідомлення по електронній пошті було відправлено:';
+$lang['raid_type'] = 'тип';
+$lang['raid_vendor'] = 'продавець';
