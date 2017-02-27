1540af470b25a2860cc8c5d216bb2909437e8658
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:05:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'Add Device';
+$lang['raid_always'] = 'foana';
+$lang['raid_app_description'] = 'Software fanafihana manome sata vaovao, tatitra sy ny fitantanana ny asa ho an&#39;ny rindrambaiko fanafihana.';
+$lang['raid_app_name'] = 'Software fanafihana Manager';
+$lang['raid_array'] = 'Array';
+$lang['raid_clean'] = 'madio';
+$lang['raid_confirm_add'] = 'Azafady hampiorina anareo te-hametraka andian-tsoratra fitaovana% s amin&#39;ny% s fanafihana voalamina hiady.';
+$lang['raid_confirm_remove'] = 'Azafady hampiorina anareo te-hanala andian-tsoratra fitaovana% s avy ao amin&#39;ny% s fanafihana voalamina hiady.';
+$lang['raid_degraded'] = 'simba';
+$lang['raid_device'] = 'Device';
+$lang['raid_email_notification'] = 'E-mail Notification';
+$lang['raid_every_30_minutes'] = 'Isaky ny 30 minitra';
+$lang['raid_every_4_hours'] = 'Isaky ny 4 ora';
+$lang['raid_every_minute'] = 'isa-minitra';
+$lang['raid_frequency'] = 'Frequency';
+$lang['raid_level'] = 'Level';
+$lang['raid_monitor_raid'] = 'Monitor fanafihana';
+$lang['raid_mount'] = 'tendrombohitra';
+$lang['raid_no_support'] = 'Tsy misy nanohana fanafihana arrays hita.';
+$lang['raid_not_degraded'] = 'Array dia tsy ao amin&#39;ny fanjakana simba.';
+$lang['raid_on_failure'] = 'On tsy fahombiazana';
+$lang['raid_raid_status'] = 'fanafihana Status';
+$lang['raid_raid_summary'] = 'fanafihana Summary';
+$lang['raid_remove'] = 'Esory';
+$lang['raid_send_mail'] = 'Alefaso E-mail Notification';
+$lang['raid_software_raid'] = 'Software fanafihana';
+$lang['raid_sync_pending'] = 'Sync miandry';
+$lang['raid_sync_scheduled'] = 'Sync voalahatra';
+$lang['raid_syncing'] = 'Sync in fandrosoana';
+$lang['raid_test_email'] = 'Alefaso Test E-mail';
+$lang['raid_test_sent_to:'] = 'Fahombiazana. E-mail fampahafantarana no nalefa tany:';
+$lang['raid_type'] = 'Type';
+$lang['raid_vendor'] = 'Mpivarotra';
