48594900dc0ad98407fd2f3b179ab9002e9ff2b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:09:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'சாதன சேர்';
+$lang['raid_always'] = 'எப்போதும்';
+$lang['raid_app_description'] = 'மென்பொருள் RAID நிலை தகவல், அறிக்கை மற்றும் மென்பொருள் RAID க்காக நிர்வாக நடவடிக்கைகளின் வழங்குகிறது.';
+$lang['raid_app_name'] = 'மென்பொருள் RAID மேலாளர்';
+$lang['raid_array'] = 'அணி';
+$lang['raid_clean'] = 'சுத்தமான';
+$lang['raid_confirm_add'] = 'நீங்கள்% கள் RAID வரிசைக்கு தடுப்பு சாதனம்% கள் சேர்க்க விரும்புகிறேன் என்று உறுதி செய்க.';
+$lang['raid_confirm_remove'] = 'நீங்கள்% கள் RAID வரிசைக்கு இருந்து தடுப்பு சாதனம்% s ஐ அகற்ற விரும்புகிறேன், தயவுசெய்து உறுதிப்படுத்தவும்.';
+$lang['raid_degraded'] = 'பற்றுகிறது';
+$lang['raid_device'] = 'சாதன';
+$lang['raid_email_notification'] = 'மின்னஞ்சல் அறிவித்தல்';
+$lang['raid_every_30_minutes'] = 'ஒவ்வொரு 30 நிமிடத்திற்கும்';
+$lang['raid_every_4_hours'] = 'ஒவ்வொரு 4 மணி';
+$lang['raid_every_minute'] = 'ஒவ்வொரு நிமிடமும்';
+$lang['raid_frequency'] = 'அதிர்வெண்';
+$lang['raid_level'] = 'நிலை';
+$lang['raid_monitor_raid'] = 'மானிட்டர் RAID ஐ';
+$lang['raid_mount'] = 'மவுண்ட்';
+$lang['raid_no_support'] = 'ஆதரிக்கின்ற RAID வரிசைகள் காணப்படும்.';
+$lang['raid_not_degraded'] = 'அணி ஒரு இழிவுபடுத்தப்பட்ட நிலையில் இல்லை.';
+$lang['raid_on_failure'] = 'தோல்வி குறித்து';
+$lang['raid_raid_status'] = 'RAID ஐ நிலைமை';
+$lang['raid_raid_summary'] = 'RAID ஐ சுருக்கம்';
+$lang['raid_remove'] = 'அகற்று';
+$lang['raid_send_mail'] = 'மின்னஞ்சல் அறிவித்தல் அனுப்பவும்';
+$lang['raid_software_raid'] = 'மென்பொருள் RAID';
+$lang['raid_sync_pending'] = 'ஒத்திசைவு நிலுவையில்';
+$lang['raid_sync_scheduled'] = 'ஒத்திசைவு திட்டமிடப்பட்டுள்ளது';
+$lang['raid_syncing'] = 'ஒத்திசைவு செயலில்';
+$lang['raid_test_email'] = 'டெஸ்ட் மின்னஞ்சல் அனுப்பவும்';
+$lang['raid_test_sent_to:'] = 'வெற்றி. மின்னஞ்சல் அறிவித்தல்கள் அனுப்பி வருகிறது:';
+$lang['raid_type'] = 'வகை';
+$lang['raid_vendor'] = 'விற்பனையாளர்';
