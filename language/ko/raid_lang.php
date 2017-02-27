890cea75d47a42b4a0b545feb1d70616b2de2afa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:04:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = '기기 추가';
+$lang['raid_always'] = '항상';
+$lang['raid_app_description'] = '소프트웨어 RAID 상태 정보,보고 및 소프트웨어 RAID에 대한 관리 작업을 제공합니다.';
+$lang['raid_app_name'] = '소프트웨어 RAID 관리자';
+$lang['raid_array'] = '정렬';
+$lang['raid_clean'] = '깨끗한';
+$lang['raid_confirm_add'] = '당신은 %의 RAID 배열에 블록 장치 % s을 (를) 추가 할 확인하시기 바랍니다.';
+$lang['raid_confirm_remove'] = '당신은 %의 RAID 어레이에서 블록 장치 % s을 (를) 제거 할 것인지 확인하시기 바랍니다.';
+$lang['raid_degraded'] = '타락한';
+$lang['raid_device'] = '장치';
+$lang['raid_email_notification'] = 'E 메일 알림';
+$lang['raid_every_30_minutes'] = '30 분마다';
+$lang['raid_every_4_hours'] = '4 시간마다';
+$lang['raid_every_minute'] = '모든 분';
+$lang['raid_frequency'] = '회수';
+$lang['raid_level'] = '수평';
+$lang['raid_monitor_raid'] = '모니터 RAID';
+$lang['raid_mount'] = '산';
+$lang['raid_no_support'] = '지원되는 RAID 어레이를 찾을 수 없습니다.';
+$lang['raid_not_degraded'] = '어레이는 성능이 저하 된 상태에 있지 않습니다.';
+$lang['raid_on_failure'] = '실패';
+$lang['raid_raid_status'] = 'RAID 상태';
+$lang['raid_raid_summary'] = 'RAID 요약';
+$lang['raid_remove'] = '풀다';
+$lang['raid_send_mail'] = '전자 메일 알림 보내기';
+$lang['raid_software_raid'] = '소프트웨어 RAID';
+$lang['raid_sync_pending'] = '대기중인 동기화';
+$lang['raid_sync_scheduled'] = '동기화 예약';
+$lang['raid_syncing'] = '진행중인 동기화';
+$lang['raid_test_email'] = '테스트 전자 메일 보내기';
+$lang['raid_test_sent_to:'] = '성공. 전자 메일 알림이 전송되었습니다 :';
+$lang['raid_type'] = '유형';
+$lang['raid_vendor'] = '공급 업체';
