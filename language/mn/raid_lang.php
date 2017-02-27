15a7529467b01b77fb4261a4cf8f2fe4ad81febc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:05:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'төхөөрөмж нэмэх';
+$lang['raid_always'] = 'үргэлж';
+$lang['raid_app_description'] = 'Програм хангамжийн RAID статус мэдээлэл, тайлагнах болон програм хангамжийн RAID-ийн захиргааны үйл ажиллагааг хангадаг.';
+$lang['raid_app_name'] = 'Програм хангамжийн RAID менежер';
+$lang['raid_array'] = 'Array';
+$lang['raid_clean'] = 'цэвэр';
+$lang['raid_confirm_add'] = 'Та% S RAID массив нь блок төхөөрөмжийн% сек нэмж хэлмээр байна баталгаажуулна уу.';
+$lang['raid_confirm_remove'] = 'Та% S RAID массиваас блок төхөөрөмжийн% S устгах хүсэж баталгаажуулна уу.';
+$lang['raid_degraded'] = 'доройтсон';
+$lang['raid_device'] = 'төхөөрөмжийн';
+$lang['raid_email_notification'] = 'И-мэйл мэдэгдэл';
+$lang['raid_every_30_minutes'] = 'Бүр 30 минут';
+$lang['raid_every_4_hours'] = 'Бүр 4 цаг';
+$lang['raid_every_minute'] = 'минут бүр';
+$lang['raid_frequency'] = 'давтамж';
+$lang['raid_level'] = 'түвшин';
+$lang['raid_monitor_raid'] = 'Monitor RAID';
+$lang['raid_mount'] = 'Mount';
+$lang['raid_no_support'] = 'Ямар ч дэмжигдсэн RAID массив олдлоо.';
+$lang['raid_not_degraded'] = 'Array нь доройтсон байдалд биш юм.';
+$lang['raid_on_failure'] = 'дутагдал нд';
+$lang['raid_raid_status'] = 'RAID байдал';
+$lang['raid_raid_summary'] = 'RAID тойм';
+$lang['raid_remove'] = 'Remove';
+$lang['raid_send_mail'] = 'И-мэйл мэдэгдэл илгээх';
+$lang['raid_software_raid'] = 'Програм хангамжийн RAID';
+$lang['raid_sync_pending'] = 'Sync хүлээгдэж байгаа';
+$lang['raid_sync_scheduled'] = 'Sync хуваарьт';
+$lang['raid_syncing'] = 'хийгдэж байгаа Sync';
+$lang['raid_test_email'] = 'Туршилтын И-мэйл илгээх';
+$lang['raid_test_sent_to:'] = 'Амжилтын. И-мэйл мэдэгдэл илгээсэн байна:';
+$lang['raid_type'] = 'Санал авах';
+$lang['raid_vendor'] = 'борлуулагч';
