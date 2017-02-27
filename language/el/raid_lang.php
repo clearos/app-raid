f7e2f77d8a0cda109077ae400c5cacded0ef30d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:01:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'Προσθέστε ΣΥΣΚΕΥΗ';
+$lang['raid_always'] = 'Πάντα';
+$lang['raid_app_description'] = 'RAID λογισμικό παρέχει πληροφορίες για την κατάσταση, την υποβολή εκθέσεων και τις διοικητικές ενέργειες για RAID λογισμικού.';
+$lang['raid_app_name'] = 'Λογισμικό διαχείρισης RAID';
+$lang['raid_array'] = 'Παράταξη';
+$lang['raid_clean'] = 'ΚΑΘΑΡΗ';
+$lang['raid_confirm_add'] = 'Παρακαλούμε επιβεβαιώστε ότι θέλετε να προσθέσετε τη συσκευή block% s σε συστοιχία RAID του% s.';
+$lang['raid_confirm_remove'] = 'Παρακαλούμε επιβεβαιώστε ότι θέλετε να καταργήσετε τη συσκευή block% s από συστοιχία RAID του% s.';
+$lang['raid_degraded'] = 'υποβαθμισμένη';
+$lang['raid_device'] = 'Συσκευή';
+$lang['raid_email_notification'] = 'Ειδοποίηση e-mail';
+$lang['raid_every_30_minutes'] = 'Κάθε 30 λεπτά';
+$lang['raid_every_4_hours'] = 'Κάθε 4 ώρες';
+$lang['raid_every_minute'] = 'Κάθε λεπτό';
+$lang['raid_frequency'] = 'Συχνότητα';
+$lang['raid_level'] = 'Επίπεδο';
+$lang['raid_monitor_raid'] = 'Monitor RAID';
+$lang['raid_mount'] = 'Βουνό';
+$lang['raid_no_support'] = 'Δεν βρέθηκαν υποστηρίζεται συστοιχίες RAID.';
+$lang['raid_not_degraded'] = 'Array δεν είναι σε υποβαθμισμένη κατάσταση.';
+$lang['raid_on_failure'] = 'σχετικά με την παράλειψη';
+$lang['raid_raid_status'] = 'RAID Status';
+$lang['raid_raid_summary'] = 'Περίληψη RAID';
+$lang['raid_remove'] = 'Αφαιρώ';
+$lang['raid_send_mail'] = 'Αποστολή ειδοποιήσεων e-mail';
+$lang['raid_software_raid'] = 'λογισμικό RAID';
+$lang['raid_sync_pending'] = 'Συγχρονισμός σε εκκρεμότητα';
+$lang['raid_sync_scheduled'] = 'Συγχρονισμός προγραμματισμένες';
+$lang['raid_syncing'] = 'Συγχρονισμός σε εξέλιξη';
+$lang['raid_test_email'] = 'Αποστολή Test E-mail';
+$lang['raid_test_sent_to:'] = 'Επιτυχία. Ειδοποίηση e-mail έχει σταλεί:';
+$lang['raid_type'] = 'Τύπος';
+$lang['raid_vendor'] = 'Προμηθευτή';
