11824985f3c189bb916cca5135e4110b3756f798
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:06:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'उपकरण थप्न';
+$lang['raid_always'] = 'सधैं';
+$lang['raid_app_description'] = 'सफ्टवेयर RAID स्थिति जानकारी, रिपोर्टिङ र लागि सफ्टवेयर RAID प्रशासनिक कार्यहरू प्रदान गर्दछ।';
+$lang['raid_app_name'] = 'सफ्टवेयर RAID प्रबन्धक';
+$lang['raid_array'] = 'एरे';
+$lang['raid_clean'] = 'साफ';
+$lang['raid_confirm_add'] = 'तपाईं% को RAID एरे गर्न ब्लक यन्त्र% को थप्न चाहनुहुन्छ पुष्टि गर्नुहोस्।';
+$lang['raid_confirm_remove'] = 'तपाईं% को RAID श्रेणीबाट ब्लक यन्त्र% को हटाउन चाहन्छु पुष्टि गर्नुहोस्।';
+$lang['raid_degraded'] = 'अपमानित';
+$lang['raid_device'] = 'उपकरण';
+$lang['raid_email_notification'] = 'ई-मेल सूचना';
+$lang['raid_every_30_minutes'] = 'हरेक 30 मिनेटमा';
+$lang['raid_every_4_hours'] = 'हरेक 4 घण्टा';
+$lang['raid_every_minute'] = 'हरेक मिनेट';
+$lang['raid_frequency'] = 'आवृत्ति';
+$lang['raid_level'] = 'स्तर';
+$lang['raid_monitor_raid'] = 'मनिटर RAID';
+$lang['raid_mount'] = 'माउन्ट';
+$lang['raid_no_support'] = 'कुनै समर्थित RAID arrays फेला परेन।';
+$lang['raid_not_degraded'] = 'एरे एक पदावनत अवस्थामा छैन।';
+$lang['raid_on_failure'] = 'विफलता मा';
+$lang['raid_raid_status'] = 'RAID स्थिति';
+$lang['raid_raid_summary'] = 'RAID सारांश';
+$lang['raid_remove'] = 'हटाउन';
+$lang['raid_send_mail'] = 'ई-मेल सूचना पठाउन';
+$lang['raid_software_raid'] = 'सफ्टवेयर RAID';
+$lang['raid_sync_pending'] = 'सिंक बाँकी';
+$lang['raid_sync_scheduled'] = 'सिंक निर्धारित';
+$lang['raid_syncing'] = 'सिंक प्रकृयामा';
+$lang['raid_test_email'] = 'परीक्षण ई-मेल पठाउन';
+$lang['raid_test_sent_to:'] = 'सफलता। ई-मेल सूचना पठाइएको छ:';
+$lang['raid_type'] = 'प्रकार';
+$lang['raid_vendor'] = 'विक्रेता';
