1379244cff3879dfa7a8b9207ac0257b4432ab99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:07:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'افزودن دستگاه';
+$lang['raid_always'] = 'همیشه';
+$lang['raid_app_description'] = 'RAID نرم افزار اطلاعات وضعیت، گزارش و اقدامات اداری برای RAID نرم افزار فراهم می کند.';
+$lang['raid_app_name'] = 'نرم افزار مدیریت RAID';
+$lang['raid_array'] = 'آرایه';
+$lang['raid_clean'] = 'تمیز';
+$lang['raid_confirm_add'] = 'لطفا تأیید کنید که می خواهم برای اضافه کردن دستگاه بلوک٪ s به٪ s را آرایه RAID.';
+$lang['raid_confirm_remove'] = 'لطفا تأیید کنید که می خواهید به حذف دستگاه بلوک٪ s از٪ s را آرایه RAID.';
+$lang['raid_degraded'] = 'تخریب';
+$lang['raid_device'] = 'دستگاه';
+$lang['raid_email_notification'] = 'هشدار از طریق ایمیل';
+$lang['raid_every_30_minutes'] = 'هر 30 دقیقه';
+$lang['raid_every_4_hours'] = 'هر 4 ساعت';
+$lang['raid_every_minute'] = 'هر دقیقه';
+$lang['raid_frequency'] = 'فرکانس';
+$lang['raid_level'] = 'سطح';
+$lang['raid_monitor_raid'] = 'RAID مانیتور';
+$lang['raid_mount'] = 'کوه';
+$lang['raid_no_support'] = 'بدون آرایه RAID پشتیبانی شده است.';
+$lang['raid_not_degraded'] = 'آرایه در حالت تنزل پیدا نکند';
+$lang['raid_on_failure'] = 'در صورت شکست';
+$lang['raid_raid_status'] = 'وضعیت RAID';
+$lang['raid_raid_summary'] = 'RAID خلاصه';
+$lang['raid_remove'] = 'برداشتن';
+$lang['raid_send_mail'] = 'ارسال هشدار از طریق ایمیل';
+$lang['raid_software_raid'] = 'RAID نرم افزار';
+$lang['raid_sync_pending'] = 'همگامسازی در حال تعلیق';
+$lang['raid_sync_scheduled'] = 'همگام سازی برنامه ریزی شده';
+$lang['raid_syncing'] = 'همگام سازی در حال پیشرفت';
+$lang['raid_test_email'] = 'ارسال تست پست الکترونیک';
+$lang['raid_test_sent_to:'] = 'موفقیت است. اطلاعرسانی ایمیلی ارسال شده است به:';
+$lang['raid_type'] = 'نوع';
+$lang['raid_vendor'] = 'فروشنده';
