9e7cd179acb865ba9847324e47c4cab52d025dc6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:08:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'උපාංගයක් එකතු කරන්න';
+$lang['raid_always'] = 'සැමවිටම';
+$lang['raid_app_description'] = 'මෘදුකාංග RAID මෘදුකාංග RAID තත්වය සඳහා තොරතුරු, වාර්තා හා පරිපාලනමය කටයුතු සම්පාදනය කරයි.';
+$lang['raid_app_name'] = 'මෘදුකාංග RAID කළමනාකරු';
+$lang['raid_array'] = 'අරා';
+$lang['raid_clean'] = 'පිරිසිදු';
+$lang['raid_confirm_add'] = 'ඔබ% s RAID අරාවට% s බ්ලොක් උපාංගයක් එකතු කිරීමට කැමැත්තෙමි සනාථ කරන්න.';
+$lang['raid_confirm_remove'] = 'ඔබ% s RAID අරාවට වලින්% s බ්ලොක් උපාංගයක් ඉවත් කිරීමට කැමති සනාථ කරන්න.';
+$lang['raid_degraded'] = 'පිරිහුණු';
+$lang['raid_device'] = 'උපාංගය';
+$lang['raid_email_notification'] = 'ඊ-තැපැල නිවේදනය';
+$lang['raid_every_30_minutes'] = 'සෑම විනාඩි 30 ක්';
+$lang['raid_every_4_hours'] = 'සෑම පැය 4';
+$lang['raid_every_minute'] = 'සෑම මිනිත්තුවක්මත්';
+$lang['raid_frequency'] = 'සංඛ්යාත';
+$lang['raid_level'] = 'මට්ටමින්';
+$lang['raid_monitor_raid'] = 'මොනිටරය, RAID';
+$lang['raid_mount'] = 'කන්ද';
+$lang['raid_no_support'] = 'සොයා සහාය RAID අයාත නැත.';
+$lang['raid_not_degraded'] = 'අරා පහත් රාජ්ය තුළ නැත.';
+$lang['raid_on_failure'] = 'අසමත් වීම';
+$lang['raid_raid_status'] = 'RAID තත්ත්වය';
+$lang['raid_raid_summary'] = 'RAID සාරාංශය';
+$lang['raid_remove'] = 'ඉවත් කරන්න';
+$lang['raid_send_mail'] = 'ඊ-තැපැල නිවේදනය යවන්න';
+$lang['raid_software_raid'] = 'මෘදුකාංග RAID';
+$lang['raid_sync_pending'] = 'විභාග සමමුහුර්ත කරන්න';
+$lang['raid_sync_scheduled'] = 'සමමුහුර්ත කරන්න නියමිත';
+$lang['raid_syncing'] = 'ප්රගතිය සමමුහුර්ත කරන්න';
+$lang['raid_test_email'] = 'ටෙස්ට් ඊ-තැපැල යවන්න';
+$lang['raid_test_sent_to:'] = 'සාර්ථකත්වය. ඊ-තැපැල නිවේදනය යවා ඇත:';
+$lang['raid_type'] = 'වර්ගය';
+$lang['raid_vendor'] = 'විකුණුම්කරුගේ';
