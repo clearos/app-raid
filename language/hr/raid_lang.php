8663999386b6dc148fc14ccec9cb0dd728d5f43f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:00:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/raid_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['raid_add_device'] = 'Dodaj uređaja';
+$lang['raid_always'] = 'Stalno';
+$lang['raid_app_description'] = 'Softver RAID pruža informacije o statusu, izvještavanje i upravne postupke za softverski RAID.';
+$lang['raid_app_name'] = 'Software RAID Manager';
+$lang['raid_array'] = 'red';
+$lang['raid_clean'] = 'Čist';
+$lang['raid_confirm_add'] = 'Potvrdite da želite dodati blok uređaja% s na% s RAID polje.';
+$lang['raid_confirm_remove'] = 'Potvrdite da želite ukloniti blok uređaja% s od% s RAID polje.';
+$lang['raid_degraded'] = 'degradirane';
+$lang['raid_device'] = 'Uređaj';
+$lang['raid_email_notification'] = 'E-mail obavijesti';
+$lang['raid_every_30_minutes'] = 'Svakih 30 minuta';
+$lang['raid_every_4_hours'] = 'Svakih 4 sata';
+$lang['raid_every_minute'] = 'Svake minute';
+$lang['raid_frequency'] = 'Frekvencija';
+$lang['raid_level'] = 'nivo';
+$lang['raid_monitor_raid'] = 'Monitor RAID';
+$lang['raid_mount'] = 'montiranje';
+$lang['raid_no_support'] = 'Nisu pronađene podržava RAID polja.';
+$lang['raid_not_degraded'] = 'Array nije u degradiranom stanju.';
+$lang['raid_on_failure'] = 'na neuspjeh';
+$lang['raid_raid_status'] = 'RAID Status';
+$lang['raid_raid_summary'] = 'RAID Sažetak';
+$lang['raid_remove'] = 'Ukloniti';
+$lang['raid_send_mail'] = 'Pošalji e-mail obavijesti';
+$lang['raid_software_raid'] = 'Software RAID';
+$lang['raid_sync_pending'] = 'Sinkronizacija na čekanju';
+$lang['raid_sync_scheduled'] = 'Sinkronizacija na rasporedu';
+$lang['raid_syncing'] = 'Sinkronizacija je u tijeku';
+$lang['raid_test_email'] = 'Pošalji Test E-mail';
+$lang['raid_test_sent_to:'] = 'Uspjeh. E-mail obavijest je poslana na:';
+$lang['raid_type'] = 'Tip';
+$lang['raid_vendor'] = 'prodavač';
